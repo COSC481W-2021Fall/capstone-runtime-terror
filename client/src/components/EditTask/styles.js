b50055e97a4bfac14509bc5f4912aa6c1f5b7bcd
Abdul-Hai <asiddiq9@emich.edu>
2021-12-03 13:31:07 -0500

    impl: Add Styling For The Create Task And Edit Task Pages
    Modified EditTask/EditTask.js to contain the appropriate classnames for the different elements. Also modified EditTask/styles.js to include the appropriate styles for the page. Also modified App.css to include the styling for the body of the entire application. GitHub Issue #38.

--- client/src/components/EditTask/styles.js
+++ client/src/components/EditTask/styles.js
@@ -2,12 +2,16 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+  
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
+      
+      
     },
   },
   paper: {
+    backgroundColor: '#F76C6C',
     padding: theme.spacing(2),
   },
   form: {
@@ -17,8 +21,21 @@ export default makeStyles((theme) => ({
   },
   buttonSubmit: {
     marginBottom: 10,
+    backgroundColor: '#24305E',
+  },
+  buttonClear:{
+    backgroundColor: '#F8E9A1',
+    color: '#24305E',
+    "&:hover": {
+      background: "#FFFF8F"
+    },
   },
   Typography: {
-    margin: '20px 0',
+    margin: '20px 450px',
+    color: '#24305E',
+  },
+  Typography: {
+    margin: '20px 450px',
+    color: '#24305E',
   },
 }));
